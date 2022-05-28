fda784a5ecbb3d308c6ad432a97bf9932bb5036c
Lennart Rolfes <lennart.rolfes@stud.hs-hannover.de>
2022-05-28 17:12:19 +0200

    sets state to TODO instead of FINISHED when submitting a question, since adaptive is not finished after submitting

--- behaviour.php
+++ behaviour.php
@@ -215,6 +215,9 @@ class qbehaviour_adaptivemoopt extends question_behaviour_with_multiple_tries {
         }
 
         $state = $this->question->grade_response_asynch($this->qa, $responsefiles ?? [], $freetextanswers);
+        if ($state == question_state::$finished){
+            $state = question_state::$todo;
+        }
 
         $pendingstep->set_state($state);
         $pendingstep->set_behaviour_var('_try', $prevtries + 1);
--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_adaptivemoopt';
 $plugin->release   = '0.1.1';
-$plugin->version   = 2022051602;
+$plugin->version   = 2022052800;
 $plugin->requires  = 2018112800;
 $plugin->maturity  = MATURITY_ALPHA;
