35049c50a7fb856bf34fb1807a7bf9b979cf59df
Lennart Rolfes <lennart.rolfes@stud.hs-hannover.de>
2022-08-07 18:53:20 +0200

    commit version.php

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'qbehaviour_adaptivemoopt';
 $plugin->release   = '0.1.1';
-$plugin->version   = 2022080702;
+$plugin->version   = 2022080703;
 $plugin->requires  = 2018112800;
 $plugin->maturity  = MATURITY_ALPHA;
