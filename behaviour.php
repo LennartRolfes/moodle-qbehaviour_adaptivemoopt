5e1f877cd69627ea2ab621a2ed84ad16e72663d1
Lennart Rolfes <lennart.rolfes@stud.hs-hannover.de>
2022-08-07 16:28:36 +0200

    fix feedback when needsgrading

--- behaviour.php
+++ behaviour.php
@@ -222,8 +222,10 @@ class qbehaviour_adaptivemoopt extends question_behaviour_with_multiple_tries {
         $state = $this->question->grade_response_asynch($this->qa, $responsefiles ?? [], $freetextanswers);
         if ($state == question_state::$finished){
             $state = question_state::$complete;
+            $pendingstep->set_behaviour_var('_completeForGrading', 1);
+        } elseif ($state == question_state::$needsgrading){
+            $pendingstep->set_behaviour_var('showGradedFeedback', 1 );
         }
-        $pendingstep->set_behaviour_var('_completeForGrading', 1);
         $pendingstep->set_state($state);
         $pendingstep->set_new_response_summary($this->question->summarise_response($response));
 
